c852d3e0464bec1c6876ad45f53ec739d325f687
velasquezmarcelo <velasquezmarcelo96@gmail.com>
2022-10-27 23:40:54 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import NavBar from './Componentes/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar/>
+      {/* <Route path={'/'} component={NavBar}/> */}
+      {/* Dos formas de poner el NavBar */}
+    </React.Fragment>
+   
   );
 }
 
---
+++ src/Componentes/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+
+
+export default function NavBar(){
+    return(
+        <React.Fragment>
+            <nav>
+              <ul>
+                <li>
+                    <NavLink to={'/home'}>Home</NavLink>
+                </li>
+                 <li>
+                    <NavLink to={'/users'}>User</NavLink>
+                </li>
+                 <li>
+                    <NavLink to={'/create'}>Create User</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/about'}>About</NavLink>
+                </li>
+              </ul>
+            </nav>
+        </React.Fragment>
+    )
+}
+
+
+
+//Crear componente
+//Import react
+//elegir class o function
+
+//diferencia entre link y navlink, navlink le podemos dar estilos su esta activo mientras que a link no se le da estilo
+//navlink tiene prop to y exact
